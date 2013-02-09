9ac8445e450147dc4606d99eec3790a806f57e74
Brenden Kokoszka <bkokoszk@nd.edu>
2013-02-09 16:12:28 -0500

    added tests

--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,7 @@
 var treap = require('../lib/treap')
   , should = require('should');
 
+
 describe('performing insertions', function() {
   var t = treap.create();
 
@@ -17,9 +18,80 @@ describe('performing insertions', function() {
     }).should.throw();
   });
 
-  it('throws an error for non-numeroc keys', function() {
+  it('throws an error for non-numeric keys', function() {
     (function(){
       t.insert('foo');
     }).should.throw();
+  });
+});
+
+
+describe('performing lookups', function() {
+  var t = treap.create();
+
+  for (var i=0; i < 1000; i++) {
+    t.insert(i, {foo: i+1, bar: i-1});
+  }
+
+  it('finds inserted nodes', function() {
+    var found = t.find(42);
+    found.key.should.equal(42);
+    found.data.foo.should.equal(43);
+    found.data.bar.should.equal(41);
+  });
+
+  it('returns null for nonexistent keys', function() {
+    var found = t.find(24601);
+    should.not.exist(found);
+  });
+
+  it('performs range lookups', function() {
+    var found = t.findRange(95, 100);
+    found.map(function(node) {
+      return node.key;
+    }).should.eql([95, 96, 97, 98, 99, 100]);
+  });
+
+  it('performs rank lookups', function() {
+    var found = t.findRank(10);
+    found.key.should.equal(9);
+  });
+});
+
+
+describe('performing removals', function() {
+  var t = treap.create();
+
+  for (var i=0; i < 1000; i++) {
+    t.insert(i);
+  }
+
+  it('removes nodes by key', function() {
+    should.exist(t.find(100));
+    t.remove(100);
+    should.not.exist(t.find(100));
+  });
+
+  it('removes nodes by reference', function() {
+    var found = t.find(200);
+    should.exist(found);
+    t.remove(found);
+    should.not.exist(t.find(200));
+  });
+});
+
+describe('traversing', function() {
+  var t = treap.create();
+  var list = [];
+
+  for (var i=0; i < 1000; i++) {
+    t.insert(i);
+    list.push(i);
+  }
+ 
+  it('traverses in order', function() {
+    t.traverse(function(node, i) {
+      node.key.should.equal(list[i]);
+    })
   });
 });
